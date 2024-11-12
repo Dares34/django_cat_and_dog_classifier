1cf80577a375a7f6d308bc250e2e5ed613f3ced6
Denis work <GorodiskiyDen@mail.ru>
2024-11-12 09:41:36 +0300

    add form and template bb_create, rubirc_bbs

--- django_cat_and_dog_classifier/bboard/views.py
+++ django_cat_and_dog_classifier/bboard/views.py
@@ -1,11 +1,33 @@
 from django.http import HttpResponse
-from .models import Bb
+from .models import Bb, Rubric
 from django.template import loader
 from django.shortcuts import render
 
+from django.urls import reverse_lazy
+from .forms import BbForm
+from django.views.generic.edit import CreateView
+
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs':bbs, 'rubrics': rubrics}
+    return render(request, 'index.html', context)
 
 
-def rubric_bbs(request):
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric = rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics':rubrics, 
+               'current_rubric':current_rubric}
+    return render(request, 'rubric_bbs.html', context)
+
+class BbCreateView(CreateView):
+    template_name = 'bb_create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
